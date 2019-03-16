6f1d275e08e8eeb1398d91e355313d054df41201
István Szirtes <istvan.szirtes@gmail.com>
2019-03-16 22:14:26 +0100

    Code style PEP8
    PEP8

--- nibe/config.py
+++ nibe/config.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+"""
+https://api.nibeuplink.com/docs/v1/Authentication
+It is up to the client owner to make sure
+these are not leaked to a third party!
+Keep it secret!
+"""
 
-# https://api.nibeuplink.com/docs/v1/Authentication
-# It is up to the client owner to make sure these are not leaked to a third party! Keep it secret!
 NIBE_UPLINK_AUTHENTICATION = {
     'username': "",
     'password': "",
     'clientId': '',
     'clientSecret': '=',
-    'systemIDs': {'': '',}
+    'systemIDs': {'': ''}
     }
